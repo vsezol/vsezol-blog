85db4cc1310dd8b53b260d32cab761f6fd65494f
Vsevolod Zolotov <vsevolod.zolotov02@mail.ru>
2021-03-22 23:53:18 +0300

    begin

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div class="max-w-md container mx-auto p-8">
+    <div class="text-3xl mb-4 flex">
+      <div class="text-gray-700 py-1">vsezol</div>
+      <div class="p-1">:</div>
+      <div class="bg-gray-700 text-white p-1">blog</div>
+    </div>
     <Nuxt />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>vsezol-blog</div>
+  <div>
+    <div v-for="(post, i) in posts" :key="i">
+      <div>
+        <div v-if="!!post.title" class="mb-1">
+          <h2 class="text-xl mb-1 font-bold">{{ post.title }}</h2>
+          <p class="text-lg">{{ post.text }}</p>
+        </div>
+        <p v-else class="text-xl mb-1">
+          {{ post.text }}
+        </p>
+        <div class="flex justify-end">
+          <div>{{ post.time }} {{ post.date }}</div>
+        </div>
+      </div>
+      <hr class="m-8" v-if="i + 1 !== posts.length" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue, Component } from 'vue-property-decorator'
 
-export default Vue.extend({})
+interface Post {
+  title: string
+  text: string
+  date: string
+  time: string
+}
+
+@Component({
+  async asyncData() {
+    const posts = require('@/posts/index.json')
+    return { posts }
+  },
+})
+export default class Index extends Vue {
+  private posts: Post[] = []
+
+  mounted() {
+    console.log(this.posts)
+  }
+}
 </script>
